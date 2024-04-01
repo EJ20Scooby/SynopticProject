f08daf8a532260c4404e774f6d3415cc693f4706
EJ20Scooby <josh.a.spurgeon200213@outlook.com>
2024-04-01 12:39:27 +0100

    More menus

--- Assets/Menus.cs
+++ Assets/Menus.cs
@@ -6,12 +6,8 @@ public class Menus : MonoBehaviour
 {
     [SerializeField] GameObject mainMenu;
     [SerializeField] GameObject pauseMenu;
-    
-    public void OpenPause()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0.0f;
-    }
+    [SerializeField] GameObject[] setupMenus;
+    int currentSetupMenu = 0;    
 
     // Update is called once per frame
     void Update()
@@ -24,12 +20,72 @@ public class Menus : MonoBehaviour
             {
                 OpenPause();
             }            
-        }        
+        }  
+        if (Input.GetKeyDown(KeyCode.PageUp))
+        {
+            NextSetup();
+        }
+        if(Input.GetKeyDown(KeyCode.PageDown))
+        {
+            LastSetup();
+        }
+    }
+
+    public void OpenPause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0.0f;
     }
 
     public void ClosePause()
     {
         pauseMenu.SetActive(false); 
         Time.timeScale = 1.0f;
+    }
+
+    public void OpenMain()
+    {
+        ClosePause();
+        mainMenu.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void CloseMain()
+    {
+        mainMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void SetUpMenuOpen()
+    {
+        currentSetupMenu = 0;
+
+        CloseMain();
+        setupMenus[currentSetupMenu].SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+    public void SetUpMenuClose()
+    {
+        setupMenus[currentSetupMenu].SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void NextSetup()
+    {
+        if(currentSetupMenu != setupMenus.Length-1) 
+        {
+            setupMenus[currentSetupMenu].SetActive(false);
+            currentSetupMenu++;
+            setupMenus[currentSetupMenu].SetActive(true);
+        }
+    }
+    public void LastSetup()
+    {
+        if (currentSetupMenu > 0)
+        {
+            setupMenus[currentSetupMenu].SetActive(false);
+            currentSetupMenu--;
+            setupMenus[currentSetupMenu].SetActive(true);
+        }
     }
 }
