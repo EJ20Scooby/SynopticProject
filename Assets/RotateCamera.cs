88ccba9a8c58e689a938a36f09d97736b071c163
EJ20Scooby <josh.a.spurgeon200213@outlook.com>
2024-04-09 16:36:21 +0100

    Basic garage scene
    Camera rotates around car in garage

---
+++ Assets/RotateCamera.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public GameObject car;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(car.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
