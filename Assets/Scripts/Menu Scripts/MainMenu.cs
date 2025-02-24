4f5f0b31cf62e0dd11ddeefc610ecac8332da2e0
EJ20Scooby <124880005+EJ20Scooby@users.noreply.github.com>
2025-02-24 12:59:29 +0000

    New Build

--- Assets/Scripts/Menu Scripts/MainMenu.cs
+++ Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -6,12 +6,34 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] GameObject mainMenu;
-    [SerializeField] GameObject optionMenu;     
-    
+    [SerializeField] GameObject optionMenu;
+    private float mphConversion = 2.237f;
+
     private void Start()
     {
         mainMenu.SetActive(true);
         optionMenu.SetActive(false);
+
+        PlayerPrefs.SetFloat("BrakePower", 1400f);
+        PlayerPrefs.SetInt("BrakeBias", 5);
+        PlayerPrefs.SetFloat("TopSpeed", 35.7632f * mphConversion);
+        PlayerPrefs.SetFloat("RideHeightFront", 0.4f);
+        PlayerPrefs.SetFloat("RideHeightRear", 0.4f);
+        PlayerPrefs.SetFloat("SpringFront", 35000f);
+        PlayerPrefs.SetFloat("SpringRear", 35000f);
+        PlayerPrefs.SetFloat("DamperFront", 4500f);
+        PlayerPrefs.SetFloat("DamperRear", 4500f);
+
+        PlayerPrefs.SetFloat("DefaultSliderPos", 0.5f);
+        PlayerPrefs.SetFloat("BrakePressureSliderPos", 0.5f);
+        PlayerPrefs.SetFloat("BrakeBiasSliderPos", 0.5f);
+        PlayerPrefs.SetFloat("FinalDriveSliderPos", 0.5f);
+        PlayerPrefs.SetFloat("RideHeightFrontSliderPos", 0.5f);
+        PlayerPrefs.SetFloat("RideHeightRearSliderPos", 0.5f);
+        PlayerPrefs.SetFloat("SpringFrontSliderPos", 0.5f);
+        PlayerPrefs.SetFloat("SpringRearSliderPos", 0.5f);
+        PlayerPrefs.SetFloat("DamperFrontSliderPos", 0.5f);
+        PlayerPrefs.SetFloat("DamperRearSliderPos", 0.5f);
     }   
 
     public void goToTrack()
